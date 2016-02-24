3f9ae826c8b2e7eb3164ec60883077ad70bef035
Spenser Bauman <sabauma@gmail.com>
2016-02-24 14:02:50 -0500

    Add script for synching results from Karst

---
+++ sync.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rsync -avz --include='result.*' --include='benchmark' --include='/*' --exclude='*' sabauman@karst.uits.iu.edu:/N/u/sabauman/Karst/src/gradual-typing-performance/benchmarks/ ~/results
