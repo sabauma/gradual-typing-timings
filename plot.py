1cb5b44279a1f65c855cd4007ac7fd0aa670cf26
Spenser Bauman <sabauma@gmail.com>
2016-03-02 17:48:05 -0500

    Change slowdown axes

--- plot.py
+++ plot.py
@@ -3,7 +3,6 @@ from collections import namedtuple
 from itertools   import izip
 
 import argparse
-import glob
 import lnm
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -63,7 +62,7 @@ def slowdown_cdf(args, data):
     plt.axvline(10, color='k')
     plt.axhline(0.7 * entries, color='c', ls='--')
     plt.xlabel('slowdown factor')
-    plt.xlim((1,10))
+    plt.xlim((0,10))
     plt.ylim((0, entries))
     plt.ylabel('number below')
     plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
