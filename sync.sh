530c2ea266c077ffc438c6b22d67956ec52f2672
Spenser Bauman <sabauma@gmail.com>
2016-03-12 01:02:38 -0500

    Add v6.2.1 data to the repo

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rsync -avz --include='result*.m2' --include='benchmark' --include='/*' --exclude='*' sabauman@karst.uits.iu.edu:/N/u/sabauman/Karst/src/gradual-typing-performance/benchmarks/ ./results
+rsync -avz --include='result*.m2' --include='benchmark' --include='/*' --exclude='*' sabauman@karst.uits.iu.edu:/N/u/sabauman/Karst/src/gradual-typing-performance-v6.2.1/benchmarks/ ./results-6.2.1
